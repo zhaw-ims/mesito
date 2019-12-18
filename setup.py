bf354586ef45e683052e6f66437b7d6eea88dbf8
Marko Ristin <marko.ristin@gmail.com>
2019-12-18 13:03:27 +0100

    Deprecate support for Python 3.5 (#7)
    Since Python 3.5 is nearing its EOL next year (2020-09-13) we decided to deprecate it. From now on, we will only support Python 3.6.

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Manufacturing',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
     license='License :: OSI Approved :: MIT License',
